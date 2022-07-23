c5b634ac0eedf9df43588a912af9a8a3dde49ef7
Hieu <hieukhi12322@gmail.com>
2022-07-23 20:02:51 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     // <BaiTapThucHanhLayout />
     // <BaiTapThuKinh />
-    // <BaiTapGioHang />
-    <BaiTapGioHangRedux />
+    <BaiTapGioHang />
+    // <BaiTapGioHangRedux />
   );
 }
 
