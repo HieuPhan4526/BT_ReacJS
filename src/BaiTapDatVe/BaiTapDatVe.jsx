90b191d82fc1c57e404ab826cd3f0d14bde38131
Hieu <hieukhi12322@gmail.com>
2022-07-29 17:22:45 +0700

    khởi tạo dự án

---
+++ src/BaiTapDatVe/BaiTapDatVe.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class BaiTapDatVe extends Component {
+  render() {
+    return <div>BaiTapDatVe</div>;
+  }
+}
---
+++ src/BaiTapDatVe/ThongTinDatVe.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class ThongTinDatVe extends Component {
+  render() {
+    return <div>ThongTinDatVe</div>;
+  }
+}
